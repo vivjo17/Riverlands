7e8b33dfc6ee4bc3f2487c4ff4d71663c03a24dd
Vivian Pupo <214867100+vivjo17@users.noreply.github.com>
2025-08-20 09:08:06 -0500

    redoing npm installations

--- assets/contacts.js
+++ assets/contacts.js
@@ -11,7 +11,7 @@ document.addEventListener("DOMContentLoaded", () => {
     };
 
     try {
-      const res = await fetch("/contact", {
+      const res = await fetch("assets/contact.js", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
